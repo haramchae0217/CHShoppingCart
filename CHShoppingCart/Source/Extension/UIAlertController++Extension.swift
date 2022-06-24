4fe7a85e9f97763adb2ba055b6288c121836c2ea
Chae Haram <djvemfla2@gmail.com>
2022-06-25 01:35:01 +0900

    [DEL] UIAlertController++Extension : 주석 삭제

--- CHShoppingCart/Source/Extension/UIAlertController++Extension.swift
+++ CHShoppingCart/Source/Extension/UIAlertController++Extension.swift
@@ -11,9 +11,7 @@ extension UIAlertController {
     static func showAlert(message: String, viewController: UIViewController) {
         let alert = UIAlertController(title: "알림", message: message , preferredStyle: .alert)
         let shoppingMoreButton = UIAlertAction(title: "더 쇼핑하기", style: .cancel)
-        let shoppingStopButton = UIAlertAction(title: "장바구니로 가기", style: .default) { (action) in
-//            guard let basketVC = UIStoryboard(name: "main", bundle: nil).instantiateViewController(withIdentifier: "BasketVC") as? ShoppingBasketViewController else { return }
-        
+        let shoppingStopButton = UIAlertAction(title: "장바구니로 가기", style: .default) { (action) in        
         }
         alert.addAction(shoppingMoreButton)
         alert.addAction(shoppingStopButton)
